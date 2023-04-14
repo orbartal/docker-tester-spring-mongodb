14f2165bfc7d7c4ee5a55dbe8d29dffdbefc86e1
Or Bartal <orbartal@gmail.com>
2023-04-14 11:33:05 -0400

    Extract MongoDockerProperties from MongoContainerUtil

---
+++ backend/src/test/java/orbartal/demo/spring/mongodb/mongo/config/MongoDockerProperties.java
@@ -0,0 +1,82 @@
+package orbartal.demo.spring.mongodb.mongo.config;
+
+//@Configuration
+public class MongoDockerProperties {
+	private int port = 27017;
+	private String host = "localhost";
+	private String dockerImage = "mongo:4.4.2";
+	private String demoDb = "demodb";
+	private String demoUserName = "demodb";
+	private String demoUserPass = "demouser";
+	private String MONGO_USER_PASS = "demopass";
+	private String MONGO_USER_NAME = "demouser";
+
+	public static MongoDockerProperties getIntance() {
+		return new MongoDockerProperties();
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+	public void setPort(int port) {
+		this.port = port;
+	}
+
+	public String getHost() {
+		return host;
+	}
+
+	public void setHost(String host) {
+		this.host = host;
+	}
+
+	public String getDockerImage() {
+		return dockerImage;
+	}
+
+	public void setDockerImage(String dockerImage) {
+		this.dockerImage = dockerImage;
+	}
+
+	public String getDemoDb() {
+		return demoDb;
+	}
+
+	public void setDemoDb(String demoDb) {
+		this.demoDb = demoDb;
+	}
+
+	public String getDemoUserName() {
+		return demoUserName;
+	}
+
+	public void setDemoUserName(String demoUserName) {
+		this.demoUserName = demoUserName;
+	}
+
+	public String getDemoUserPass() {
+		return demoUserPass;
+	}
+
+	public void setDemoUserPass(String demoUserPass) {
+		this.demoUserPass = demoUserPass;
+	}
+
+	public String getMONGO_USER_PASS() {
+		return MONGO_USER_PASS;
+	}
+
+	public void setMONGO_USER_PASS(String mONGO_USER_PASS) {
+		MONGO_USER_PASS = mONGO_USER_PASS;
+	}
+
+	public String getMONGO_USER_NAME() {
+		return MONGO_USER_NAME;
+	}
+
+	public void setMONGO_USER_NAME(String mONGO_USER_NAME) {
+		MONGO_USER_NAME = mONGO_USER_NAME;
+	}
+
+}
--- backend/src/test/java/orbartal/demo/spring/mongodb/mongo/container/MongoContainerUtil.java
+++ backend/src/test/java/orbartal/demo/spring/mongodb/mongo/container/MongoContainerUtil.java
@@ -7,20 +7,18 @@ import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.utility.DockerImageName;
 
+import orbartal.demo.spring.mongodb.mongo.config.MongoDockerProperties;
+
 public class MongoContainerUtil {
 
-	private static final String MONGO_IMAGE = "mongo:4.4.2";
-	private static final String MONGO_DB_NAME = "demodb";
-	private static final int MONGO_PORT = 27017;
-	private static final String MONGO_USER_PASS = "demopass";
-	private static final String MONGO_USER_NAME = "demouser";
-	
-	private static final DockerImageName MONGO_DOCKER_IMAGE = DockerImageName.parse(MONGO_IMAGE);
+	private static final MongoDockerProperties properties = MongoDockerProperties.getIntance();
 
 	@SuppressWarnings("resource")
 	public static MongoDBContainer buildMongoDbContainer() {
-		return new MongoDBContainer(MONGO_DOCKER_IMAGE)
-		.withEnv("MONGO_INITDB_DATABASE", MONGO_DB_NAME);
+		String demoDb = properties.getDemoDb();
+		String dockerImage = properties.getDockerImage();
+		DockerImageName image = DockerImageName.parse(dockerImage);
+		return new MongoDBContainer(image).withEnv("MONGO_INITDB_DATABASE", demoDb);
 	}
 
 	public static void setProperties(DynamicPropertyRegistry registry, GenericContainer<?> mongoDBContainer) {
@@ -28,17 +26,17 @@ public class MongoContainerUtil {
 		int mapPort0 = getMongoPort(mongoDBContainer);
 		registry.add("spring.data.mongodb.host", ()->host);
 		registry.add("spring.data.mongodb.port", ()->mapPort0);
-		registry.add("spring.data.mongodb.database", ()->MONGO_DB_NAME);
+		registry.add("spring.data.mongodb.database", ()->properties.getDemoDb());
 	}
 	
 	@SuppressWarnings("resource")
 	public static GenericContainer<?> buildMongoDbContainerWithSecurity() {
 		return new GenericContainer<>
-				(DockerImageName.parse(MONGO_IMAGE))
-				.withEnv("MONGO_INITDB_ROOT_USERNAME", MONGO_USER_NAME)
-				.withEnv("MONGO_INITDB_ROOT_PASSWORD", MONGO_USER_PASS)
-				.withEnv("MONGO_INITDB_DATABASE", MONGO_DB_NAME)
-				.withExposedPorts(MONGO_PORT);
+				(DockerImageName.parse(properties.getDockerImage()))
+				.withEnv("MONGO_INITDB_ROOT_USERNAME", properties.getDemoUserName())
+				.withEnv("MONGO_INITDB_ROOT_PASSWORD", properties.getDemoUserPass())
+				.withEnv("MONGO_INITDB_DATABASE", properties.getDemoDb())
+				.withExposedPorts(properties.getPort());
 	}
 	
 	public static void setPropertiesWithSecurity(DynamicPropertyRegistry registry, GenericContainer<?> mongoDBContainer) {
@@ -46,9 +44,9 @@ public class MongoContainerUtil {
 		int mapPort0 = getMongoPort(mongoDBContainer);
 		registry.add("spring.data.mongodb.host", ()->host);
 		registry.add("spring.data.mongodb.port", ()->mapPort0);
-		registry.add("spring.data.mongodb.database", ()->MONGO_DB_NAME);
-		registry.add("spring.data.mongodb.username", ()->MONGO_USER_NAME);
-		registry.add("spring.data.mongodb.password", ()->MONGO_USER_PASS);
+		registry.add("spring.data.mongodb.database", ()->properties.getDemoDb());
+		registry.add("spring.data.mongodb.username", ()->properties.getDemoUserName());
+		registry.add("spring.data.mongodb.password", ()->properties.getDemoUserPass());
 		registry.add("spring.data.mongodb.authentication-database", ()->"admin");
 	}
 
