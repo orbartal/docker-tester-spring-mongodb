d7063a5c2c770ce56d6598e218a9954e6ecd6e89
Or Bartal <orbartal@gmail.com>
2023-04-18 12:37:55 -0400

    Refactor. Rename set from  dbrs to rsone in compose/t2

--- compose/t2/start.sh
+++ compose/t2/start.sh
@@ -2,4 +2,4 @@ docker-compose up -d
 
 sleep 10
 
-docker exec -it mongo.one.db mongosh --eval "rs.initiate({_id:'dbrs', members: [{_id:0, host: 'mongo.one.db'},{_id:1, host: 'mongo.two.db'},{_id:2, host: 'mongo.three.db'}]})"
+docker exec -it mongo.one.db mongosh --eval "rs.initiate({_id:'rsone', members: [{_id:0, host: 'mongo.one.db'},{_id:1, host: 'mongo.two.db'},{_id:2, host: 'mongo.three.db'}]})"
